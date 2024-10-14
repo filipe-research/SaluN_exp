9528da30ed974227c7f99f1381224ef58d9e9e6b
filiperobotic <filipe.rolim@gmail.com>
2024-10-14 15:28:04 -0300

    db2

--- Classification/dataset.py
+++ Classification/dataset.py
@@ -599,6 +599,7 @@ def cifar10_dataloaders(
 
     noise_file = f'cifar10_{noise_rate}_sym.json'
     if os.path.exists(noise_file):
+            # import pdb; pdb.set_trace()
             noise = json.load(open(noise_file, "r"))
             noise_labels = noise['noise_labels']
             
@@ -648,7 +649,7 @@ def cifar10_dataloaders(
         #noise = {'noise_labels': noise_labels, 'open_noise': self.open_noise, 'closed_noise': self.closed_noise}
         noise = {'noise_labels': noise_labels,  'closed_noise': closed_noise}
         print("save noise to %s ..." % noise_file)
-        import pdb; pdb.set_trace()
+        
         json.dump(noise, open(noise_file, "w"))
         # self.cifar_label = noise_labels
         # self.open_id = np.array(self.open_noise)[:, 0] if len(self.open_noise) !=0 else None
