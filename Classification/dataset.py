46f85d0b864b50f4cc5d078b6472cf6e976242bd
filiperobotic <filipe.rolim@gmail.com>
2025-06-04 11:13:31 -0300

    okok

--- Classification/dataset.py
+++ Classification/dataset.py
@@ -254,7 +254,7 @@ def cifar100_dataloaders(
             #train_set_copy.targets[train_idx] = np.array(noise_labels)
             train_set_copy.targets = np.array(noise_labels)
             train_set.targets = train_set_copy.targets
-            import pdb; pdb.set_trace()
+            # import pdb; pdb.set_trace()
             
         
     else:
