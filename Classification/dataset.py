a4daa9ba5ab5361149308aa7f92ac7423ec4c9b5
filiperobotic <filipe.rolim@gmail.com>
2025-06-19 10:50:31 -0300

    debug

--- Classification/dataset.py
+++ Classification/dataset.py
@@ -1004,7 +1004,7 @@ def cifar100_idn_dataloaders(
 
     # Aplicar rótulos ruidosos
     train_set.targets = np.array(noise_labels)
-    # import pdb; pdb.set_trace()
+    import pdb; pdb.set_trace()
     if indexes_to_replace is not None or indexes_to_replace == 450:
         replace_indexes(
             dataset=train_set,
@@ -1165,7 +1165,7 @@ def cifar10_openset_dataloaders(
     train_set.data = train_set_copy.data[train_idx]
     train_set.targets = train_set_copy.targets[train_idx]
 
-    import pdb; pdb.set_trace()
+    
     if indexes_to_replace is not None or indexes_to_replace == 450:
         replace_indexes(
             dataset=train_set,
