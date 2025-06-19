2bb725ca743cfacc513722ad5965a409e2c5ae48
filiperobotic <filipe.rolim@gmail.com>
2025-06-19 11:40:06 -0300

    debug

--- Classification/main_forget.py
+++ Classification/main_forget.py
@@ -78,6 +78,7 @@ def main():
             # import pdb; pdb.set_trace()
             marked = forget_dataset.targets < 0
             #forget_dataset.data = forget_dataset.data[marked]
+            import pdb; pdb.set_trace()
             forget_dataset.image_paths = forget_dataset.image_paths[marked]
             forget_dataset.targets = -forget_dataset.targets[marked] - 1
             forget_loader = replace_loader_dataset(
